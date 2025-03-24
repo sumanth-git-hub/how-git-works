4111ca85b0ccde600e6f3a5042d441a67f739fe9
Sumanth Suresh <sumanthss025@gmail.com>
2025-03-24 07:24:24 +0530

    Added background color change function on javascript file

--- contact-script.js
+++ contact-script.js
@@ -1,18 +1,13 @@
-const colorButton = document.getElementById("color-button");
+const escalationButton = document.querySelector(".escalation-open-button");
+const modal = document.querySelector(".modal");
+const closeModal = document.getElementById("close-modal");
 
-colorButton.addEventListener("click", (e) => {
-    colorButton.parentElement.classList.toggle("change-bg");
+escalationButton.addEventListener("click", (e) => {
+    escalationButton.parentElement.classList.add("change-bg");
+    modal.classList.add("open-modal");
+});
+
+closeModal.addEventListener("click", (e) => {
+    escalationButton.parentElement.classList.remove("change-bg");
+    modal.classList.remove("open-modal");
 })
-
-
-// const escalationButton = document.querySelector(".escalation-open-button");
-// const modal = document.querySelector(".modal");
-// const closeModal = document.getElementById("close-modal");
-
-// escalationButton.addEventListener("click", (e) => {
-//     modal.classList.add("open-modal");
-// });
-
-// closeModal.addEventListener("click", (e) => {
-//     modal.classList.remove("open-modal");
-// })
