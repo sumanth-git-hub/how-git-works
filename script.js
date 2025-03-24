7a7a2ccd58e9e547f17ddd267282ee18f5630ee6
Sumanth Suresh <sumanthss025@gmail.com>
2025-03-24 07:35:12 +0530

    Again inseted the script file

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-const colorButton = document.getElementById("color-button");
+const backgroundColor = document.getElementById("color-button");
 
-colorButton.addEventListener("click", (e) => {
-    colorButton.parentElement.classList.toggle("change-bg");
+backgroundColor.addEventListener("click", (e) => {
+    backgroundColor.parentElement.classList.toggle("change-bg");
 })
-
-
-// const escalationButton = document.querySelector(".escalation-open-button");
-// const modal = document.querySelector(".modal");
-// const closeModal = document.getElementById("close-modal");
-
-// escalationButton.addEventListener("click", (e) => {
-//     modal.classList.add("open-modal");
-// });
-
-// closeModal.addEventListener("click", (e) => {
-//     modal.classList.remove("open-modal");
-// })
