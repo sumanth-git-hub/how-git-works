a86bbdf33f6b3e12e1eab0f2fe2919d37503b3f4
Sumanth Suresh <sumanthss025@gmail.com>
2025-03-24 07:05:27 +0530

    Commented the Escalation Code

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const escalationButton = document.querySelector(".escalation-open-button");
-const modal = document.querySelector(".modal");
-const closeModal = document.getElementById("close-modal");
+const colorButton = document.getElementById("color-button");
 
-escalationButton.addEventListener("click", (e) => {
-    modal.classList.add("open-modal");
-});
-
-closeModal.addEventListener("click", (e) => {
-    modal.classList.remove("open-modal");
+colorButton.addEventListener("click", (e) => {
+    colorButton.parentElement.classList.toggle("change-bg");
 })
+
+
+// const escalationButton = document.querySelector(".escalation-open-button");
+// const modal = document.querySelector(".modal");
+// const closeModal = document.getElementById("close-modal");
+
+// escalationButton.addEventListener("click", (e) => {
+//     modal.classList.add("open-modal");
+// });
+
+// closeModal.addEventListener("click", (e) => {
+//     modal.classList.remove("open-modal");
+// })
