d894299070371cef450ea03d610b555ce28e4ebe
Sumanth Suresh <sumanthss025@gmail.com>
2025-03-25 08:03:24 +0530

    Added set timeout function on script file

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const backgroundColor = document.getElementById("color-button");
 
 backgroundColor.addEventListener("click", (e) => {
     backgroundColor.parentElement.classList.toggle("change-bg");
-})
+});
+
+setTimeout(() => {
+    console.log(backgroundColor)
+}, 3000);
